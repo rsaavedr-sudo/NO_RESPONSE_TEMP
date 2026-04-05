793ddbe902e673f27c0a97bcd9ed436ece452ae2
rsaavedr-sudo <rsaavedr@gmail.com>
2026-04-05 17:20:27 -0300

    feat: Update Vite config and dependencies
    Adjusts the Vite configuration to use `fileURLToPath` for resolving the alias path, improving clarity and compatibility with ES modules. Also, updates package dependencies to ensure the latest versions are used.

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
+import { fileURLToPath, URL } from 'node:url';
 import {defineConfig, loadEnv} from 'vite';
 
 export default defineConfig(({mode}) => {
@@ -12,7 +12,7 @@ export default defineConfig(({mode}) => {
     },
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, '.'),
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
       },
     },
     server: {
